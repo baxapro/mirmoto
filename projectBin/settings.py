e767f1712a63bce3a7bdca0f05a9dfa86e0472d6
baxa0 <bakdaulet.tursynkul@narxoz.kz>
2023-02-19 17:58:16 +0600

    2 ыы

--- projectBin/settings.py
+++ projectBin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nxtlv@w19llwn92d%$*d%o$7(p-w30c8e4kuue(6lod%fzcl7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
