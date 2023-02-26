6db1b682e0e31700494adb61c59b1d4776f0de78
baxa0 <bakdaulet.tursynkul@narxoz.kz>
2023-02-26 20:26:41 +0600

    3

--- men/urls.py
+++ men/urls.py
@@ -4,6 +4,15 @@ from .views import *
 
 urlpatterns = [
     path('',index, name = 'home'),
-    path('about', about, name='about'),
+    path('about/', about, name='about'),
+    path('addproduct/',addproduct, name= 'add_product'),
+    path('contact/',contact, name='contact'),
+    path('product/',product, name= 'product'),
+    path('qyzmet/',qyzmet, name= 'qyzmet'),
+    path('registration/',registration, name= 'registration'),
+    path('login/',login, name= 'login'),
+    path('post/<int:post_id>/',show_post,name = 'post'),
+    path('category/<int:cat_id>/',show_category,name = 'category')
+
 
 ]
--- men/views.py
+++ men/views.py
@@ -5,26 +5,58 @@ from .models import *
 
 # Create your views here.
 
-menu = ['Home','About me','categories']
+menu = [{'title': 'Басты бет','url_name': 'home'},
+        {'title': 'Біз туралы','url_name': 'about'},
+        {'title': 'Мопед қосу','url_name': 'add_product'},
+        {'title': 'Қызметтер','url_name': 'qyzmet'},
+        {'title': 'Заттар', 'url_name': 'product'},
+        {'title': 'Байланыс', 'url_name': 'contact'}
+
+        ]
+regis = [{'title':'Тіркелу','url_name':'registration'},
+         {'title':'Кіру','url_name':'login'}
+         ]
 
 def index(request):
     posts = Men.objects.all()
-    return render(request, 'men/index.html', {'posts':posts,'menu': menu,'title':'Index page'})
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'cats':cats,
+        'regis':regis,
+        'title': 'Index page',
+        'cat_selected': 0,
+    }
+    return render(request, 'men/index.html',context = context)
 
 def about(request):
     return render(request, 'men/about.html',{'menu': menu,'title':'About page'})
 
+def addproduct(request):
+    return HttpResponse('Add Product')
 
-def categories(request, catid):
-    # if request.POST:
-    #    print(request.POST)
+def product(request):
+    return HttpResponse('Product')
 
-    return HttpResponse(f"<img src='https://www.unisender.com/wp-content/uploads/2020/11/01-2-768x346.png' ><p>{catid}</p>")
+def contact(request):
+    return HttpResponse('Contact')
 
-def archive(request,year):
-    if int(year)>2020:
-        return redirect('home')
-    return HttpResponse(f"<h1>History {year}</h1>")
+def qyzmet(request):
+    return HttpResponse('Qyzmet')
+
+def registration(request):
+    return HttpResponse('Registration')
+
+def login(request):
+    return HttpResponse('Login')
+
+
+def show_post(request,post_id):
+    return HttpResponse(f"Id={post_id}")
+
+def show_category(request,cat_id):
+    return HttpResponse(f"Id={cat_id}")
 
 def pageNotFound(request,exception):
     return render(request,'men/404.html')
@@ -34,4 +66,6 @@ def Error(request,exception):
 
 def Error400(request,excepion):
     return render(request,'men/400.html')
+
+
 
